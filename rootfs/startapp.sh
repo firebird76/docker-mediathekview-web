b8ead18ca78d4accf4e0b24e85a49ea588532162
Firebird76 <dev@scharlewsky.de>
2026-06-08 08:53:53 +0200

    erstmal mit gui

--- rootfs/startapp.sh
+++ rootfs/startapp.sh
@@ -1,23 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env sh
+
 export HOME=/config
 
-#exec /usr/local/bin/MediathekView
-
-# Schleife für den 6-Stunden-Rhythmus
-while true; do
-    echo "=================================================="
-    echo "Starte automatischen MediathekView-Suchlauf: $(date)"
-    echo "=================================================="
-   
-    # Sollte 'mediathekview' nicht gefunden werden, kannst du es durch 
-    # den exakten Pfad (z.B. /usr/bin/mediathekview) ersetzen.
-    /usr/local/bin/MediathekView -dq
-    
-    echo "--------------------------------------------------"
-    echo "Downloads abgeschlossen. MediathekView beendet."
-    echo "Nächster Suchlauf in 6 Stunden..."
-    echo "=================================================="
-    
-    # 6 Stunden warten
-    sleep 21600
-done
+exec /usr/local/bin/MediathekView
